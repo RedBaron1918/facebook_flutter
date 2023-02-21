d7b840527891189cd8e3abefe4f8f088d1455073
RedBaron1918 <asatianikotiko1@gmail.com>
2023-02-21 04:07:55 -0800

    added Tabs

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,55 +2,88 @@ import 'package:facebook_page/data/data.dart';
 import 'package:facebook_page/screens/image_screen.dart';
 import 'package:facebook_page/widgets/circle_button.dart';
 import 'package:facebook_page/widgets/post_container.dart';
+import 'package:facebook_page/widgets/profile_avatar.dart';
 import 'package:facebook_page/widgets/stories.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import '../widgets/create_post_container.dart';
 
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        systemOverlayStyle: SystemUiOverlayStyle.light,
-        backgroundColor: Colors.white,
-        title: const Text(
-          "facebook",
-          style: TextStyle(
-              color: Color.fromARGB(255, 1, 115, 168),
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-              letterSpacing: -1.2,
+   const int tabsCount = 6;
+
+    return DefaultTabController(
+      initialIndex: 1,
+      length: tabsCount,
+      child: Scaffold(
+        appBar: AppBar(
+          systemOverlayStyle: SystemUiOverlayStyle.light,
+          backgroundColor: Colors.white,
+          title: const Text(
+            "facebook",
+            style: TextStyle(
+                color: Color.fromARGB(255, 1, 115, 168),
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                letterSpacing: -1.2,
+            ),
           ),
+          centerTitle: false,
+          bottom:  TabBar(
+            labelColor: Colors.black,
+            tabs:   [
+                const Tab(
+                icon:  Icon(Icons.home),
+                
+              ),
+              const Tab(
+                icon:  Icon(Icons.people),
+              ),
+              const Tab(
+                icon:  Icon(MdiIcons.television),
+              ),
+              const Tab(
+                icon:  Icon(Icons.shopify),
+              ),
+              const Tab(
+                icon:  Icon(MdiIcons.bell),
+              ),
+              Tab(
+                icon:  ProfileAvatar(imageUrl: currentUser.imageUrl),
+              ),
+            ],
+          ),
+          actions: [
+            CircleButton(
+              icon: Icons.search,
+              iconSize: 28,
+              press: () {},
+            ),
+            CircleButton(
+              icon: MdiIcons.facebookMessenger,
+              iconSize: 30,
+              press: () {},
+            ),
+          ],
         ),
-        centerTitle: false,
-        actions: [
-          CircleButton(
-            icon: Icons.search,
-            iconSize: 28,
-            press: () {},
-          ),
-          CircleButton(
-            icon: MdiIcons.facebookMessenger,
-            iconSize: 30,
-            press: () {},
-          ),
-        ],
-      ),
-      body: ListView(
-       physics: const BouncingScrollPhysics(),
-        children: [
-          CreatePostContaner(currentUser: currentUser),
-          const SizedBox(height: 10),
-          Stories(currentUser: currentUser, stories: stories),
-          const SizedBox(height: 5),
-          Column(
-            children: posts.map((post) => PostContainer(post: post,call: ()=> Navigator.of(context).push(MaterialPageRoute(builder: (context)=> ImageScreen(story: post,))),)).toList(),
-          ),
-        ],
+        body: ListView(
+         physics: const BouncingScrollPhysics(
+                parent: AlwaysScrollableScrollPhysics()),
+          children: [
+            CreatePostContaner(currentUser: currentUser),
+            const SizedBox(height: 10),
+            Stories(currentUser: currentUser, stories: stories),
+            const SizedBox(height: 5),
+            Column(
+              children: posts.map((post) => PostContainer(post: post,call: ()=> Navigator.of(context).push(MaterialPageRoute(builder: (context)=> ImageScreen(story: post,))),)).toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/image_screen.dart
+++ lib/screens/image_screen.dart
@@ -1,4 +1,4 @@
-import 'package:facebook_page/models/story.dart';
+import 'package:facebook_page/widgets/profile_avatar.dart';
 import 'package:flutter/material.dart';
 
 
@@ -10,6 +10,17 @@ class ImageScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return Scaffold(
+      appBar: AppBar(
+        title: Text(story.user.name,style: const TextStyle(fontWeight: FontWeight.bold),),
+        backgroundColor: Colors.grey[400],
+        leading: Padding(
+          padding: const EdgeInsets.all(12),
+          child: ProfileAvatar(
+                    imageUrl: story.user.imageUrl,
+          ),
+        ),
+        
+      ),
       body: GestureDetector(
         onTap: () {
           Navigator.pop(context);
