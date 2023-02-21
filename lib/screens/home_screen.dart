f5268ba37b3d099f4485d85316316bf60904ef60
RedBaron1918 <asatianikotiko1@gmail.com>
2023-02-22 03:00:03 +0400

    actually did not break anything

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,83 +1,94 @@
 import 'package:facebook_page/data/data.dart';
 import 'package:facebook_page/screens/image_screen.dart';
+import 'package:facebook_page/widgets/bottom_navigation.dart';
 import 'package:facebook_page/widgets/circle_button.dart';
 import 'package:facebook_page/widgets/post_container.dart';
-import 'package:facebook_page/widgets/profile_avatar.dart';
 import 'package:facebook_page/widgets/stories.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import '../widgets/create_post_container.dart';
+import '../widgets/scroll_to_hide.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late final ScrollController controller = ScrollController();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    const iconColor = Color.fromARGB(255, 1, 115, 168);
+
     return Scaffold(
-      appBar: AppBar(
-        systemOverlayStyle: SystemUiOverlayStyle.light,
-        backgroundColor: Colors.white,
-        title: const Text(
-          "facebook",
-          style: TextStyle(
-            color: Color.fromARGB(255, 1, 115, 168),
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-            letterSpacing: -1.2,
-          ),
-        ),
-        centerTitle: false,
-        actions: [
-          CircleButton(
-            icon: Icons.search,
-            iconSize: 28,
-            press: () {},
-          ),
-          CircleButton(
-            icon: MdiIcons.facebookMessenger,
-            iconSize: 30,
-            press: () {},
-          ),
-        ],
+      bottomNavigationBar: ScrollToHideWidget(
+        controller: controller,
+        child: const MyBottomNavigationBar(),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        type: BottomNavigationBarType.fixed,
-        items: [
-          const BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
-          const BottomNavigationBarItem(
-              icon: Icon(Icons.people), label: "people"),
-          const BottomNavigationBarItem(
-              icon: Icon(MdiIcons.television), label: "television"),
-          const BottomNavigationBarItem(
-              icon: Icon(Icons.shopify), label: "shop"),
-          const BottomNavigationBarItem(
-              icon: Icon(MdiIcons.bell), label: "bell"),
-          BottomNavigationBarItem(
-              icon: ProfileAvatar(imageUrl: currentUser.imageUrl),
-              label: "bell"),
-        ],
-      ),
-      body: ListView(
-        physics: const BouncingScrollPhysics(
-            parent: AlwaysScrollableScrollPhysics()),
-        children: [
-          CreatePostContaner(currentUser: currentUser),
-          const SizedBox(height: 10),
-          Stories(currentUser: currentUser, stories: stories),
-          const SizedBox(height: 5),
-          Column(
-            children: posts
-                .map((post) => PostContainer(
-                      post: post,
-                      call: () => Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => ImageScreen(
-                                story: post,
-                              ))),
-                    ))
-                .toList(),
+      body: CustomScrollView(
+        controller: controller,
+        slivers: [
+          SliverAppBar(
+            systemOverlayStyle: SystemUiOverlayStyle.light,
+            backgroundColor: Colors.white,
+            floating: true,
+            title: const Text(
+              'facebook',
+              style: TextStyle(
+                color: iconColor,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                letterSpacing: -1.2,
+              ),
+            ),
+            centerTitle: false,
+            actions: [
+              CircleButton(
+                icon: Icons.search,
+                iconSize: 28,
+                press: () {},
+              ),
+              CircleButton(
+                icon: MdiIcons.facebookMessenger,
+                iconSize: 30,
+                press: () {},
+              ),
+            ],
+          ),
+          SliverToBoxAdapter(
+            child: CreatePostContaner(currentUser: currentUser),
+          ),
+          const SliverToBoxAdapter(
+            child: SizedBox(height: 10),
+          ),
+          SliverToBoxAdapter(
+            child: Stories(currentUser: currentUser, stories: stories),
+          ),
+          const SliverToBoxAdapter(
+            child: SizedBox(height: 5),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                final post = posts[index];
+                return PostContainer(
+                  post: post,
+                  call: () => Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => ImageScreen(story: post))),
+                );
+              },
+              childCount: posts.length,
+            ),
           ),
         ],
       ),
