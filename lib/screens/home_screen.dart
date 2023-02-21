bc24e06d98d662c70a95816c3ae137d3a40606f9
RedBaron1918 <asatianikotiko1@gmail.com>
2023-02-22 00:22:28 +0400

    before i break everything

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,81 +9,77 @@ import 'package:flutter/services.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import '../widgets/create_post_container.dart';
 
-
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-   const int tabsCount = 6;
-
-    return DefaultTabController(
-      initialIndex: 1,
-      length: tabsCount,
-      child: Scaffold(
-        appBar: AppBar(
-          systemOverlayStyle: SystemUiOverlayStyle.light,
-          backgroundColor: Colors.white,
-          title: const Text(
-            "facebook",
-            style: TextStyle(
-                color: Color.fromARGB(255, 1, 115, 168),
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                letterSpacing: -1.2,
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+        backgroundColor: Colors.white,
+        title: const Text(
+          "facebook",
+          style: TextStyle(
+            color: Color.fromARGB(255, 1, 115, 168),
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+            letterSpacing: -1.2,
           ),
-          centerTitle: false,
-          bottom:  TabBar(
-            labelColor: Colors.black,
-            tabs:   [
-                const Tab(
-                icon:  Icon(Icons.home),
-                
-              ),
-              const Tab(
-                icon:  Icon(Icons.people),
-              ),
-              const Tab(
-                icon:  Icon(MdiIcons.television),
-              ),
-              const Tab(
-                icon:  Icon(Icons.shopify),
-              ),
-              const Tab(
-                icon:  Icon(MdiIcons.bell),
-              ),
-              Tab(
-                icon:  ProfileAvatar(imageUrl: currentUser.imageUrl),
-              ),
-            ],
+        ),
+        centerTitle: false,
+        actions: [
+          CircleButton(
+            icon: Icons.search,
+            iconSize: 28,
+            press: () {},
           ),
-          actions: [
-            CircleButton(
-              icon: Icons.search,
-              iconSize: 28,
-              press: () {},
-            ),
-            CircleButton(
-              icon: MdiIcons.facebookMessenger,
-              iconSize: 30,
-              press: () {},
-            ),
-          ],
-        ),
-        body: ListView(
-         physics: const BouncingScrollPhysics(
-                parent: AlwaysScrollableScrollPhysics()),
-          children: [
-            CreatePostContaner(currentUser: currentUser),
-            const SizedBox(height: 10),
-            Stories(currentUser: currentUser, stories: stories),
-            const SizedBox(height: 5),
-            Column(
-              children: posts.map((post) => PostContainer(post: post,call: ()=> Navigator.of(context).push(MaterialPageRoute(builder: (context)=> ImageScreen(story: post,))),)).toList(),
-            ),
-          ],
-        ),
+          CircleButton(
+            icon: MdiIcons.facebookMessenger,
+            iconSize: 30,
+            press: () {},
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          const BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+          const BottomNavigationBarItem(
+              icon: Icon(Icons.people), label: "people"),
+          const BottomNavigationBarItem(
+              icon: Icon(MdiIcons.television), label: "television"),
+          const BottomNavigationBarItem(
+              icon: Icon(Icons.shopify), label: "shop"),
+          const BottomNavigationBarItem(
+              icon: Icon(MdiIcons.bell), label: "bell"),
+          BottomNavigationBarItem(
+              icon: ProfileAvatar(imageUrl: currentUser.imageUrl),
+              label: "bell"),
+        ],
+      ),
+      body: ListView(
+        physics: const BouncingScrollPhysics(
+            parent: AlwaysScrollableScrollPhysics()),
+        children: [
+          CreatePostContaner(currentUser: currentUser),
+          const SizedBox(height: 10),
+          Stories(currentUser: currentUser, stories: stories),
+          const SizedBox(height: 5),
+          Column(
+            children: posts
+                .map((post) => PostContainer(
+                      post: post,
+                      call: () => Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => ImageScreen(
+                                story: post,
+                              ))),
+                    ))
+                .toList(),
+          ),
+        ],
       ),
     );
   }
