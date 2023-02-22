86b982b7cc5e8e9ec2939964c89a3883f0a8daf6
RedBaron1918 <asatianikotiko1@gmail.com>
2023-02-22 05:00:17 -0800

    gonna continue home

--- lib/screens/image_screen.dart
+++ lib/screens/image_screen.dart
@@ -1,6 +1,7 @@
 import 'package:facebook_page/models/story.dart';
 import 'package:facebook_page/widgets/profile_avatar.dart';
 import 'package:flutter/material.dart';
+
 class ImageScreen extends StatelessWidget {
   final Story story;
 
@@ -8,39 +9,50 @@ class ImageScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Dismissible(key: Key(story.user.name), direction: DismissDirection.down,onDismissed:(_)=> Navigator.pop(context) ,child: 
-    Scaffold(
-      backgroundColor: Colors.black12,
-      appBar: AppBar(
-        title: Text(
-          story.user.name,
-          style: const TextStyle(fontWeight: FontWeight.bold),
-        ),
-        backgroundColor: Colors.black38,
-        leading: Padding(
-          padding: const EdgeInsets.all(12),
-          child: ProfileAvatar(
-            imageUrl: story.user.imageUrl,
+    return Dismissible(
+        key: Key(story.user.name),
+        onDismissed: (direction) {
+          switch (direction) {
+              case DismissDirection.down:
+              Navigator.pop(context);
+              break;
+              
+
+            default:
+          }
+          Navigator.pop(context);
+        },
+        child: Scaffold(
+          backgroundColor: Colors.black12,
+          appBar: AppBar(
+            title: Text(
+              story.user.name,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            backgroundColor: Colors.black38,
+            leading: Padding(
+              padding: const EdgeInsets.all(12),
+              child: ProfileAvatar(
+                imageUrl: story.user.imageUrl,
+              ),
+            ),
+            actions: [
+              Padding(
+                padding: const EdgeInsetsDirectional.symmetric(horizontal: 10),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Icon(Icons.close),
+                ),
+              ),
+            ],
           ),
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsetsDirectional.symmetric(horizontal: 10),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: const Icon(Icons.close),
+          body: Center(
+            child: Image.network(
+              story.imageUrl,
             ),
           ),
-        ],
-      ),
-      body: Center(
-        child: Image.network(
-          story.imageUrl,
-        ),
-      ),
-    )
-    );
+        ));
   }
 }
