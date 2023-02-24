3f5f24c0fd9783bccb4711d990372e5b20cb5e9c
RedBaron1918 <asatianikotiko1@gmail.com>
2023-02-24 02:37:41 -0800

    finally finished it

--- lib/screens/image_screen.dart
+++ lib/screens/image_screen.dart
@@ -2,7 +2,6 @@ import 'package:facebook_page/data/data.dart';
 import 'package:facebook_page/models/story.dart';
 import 'package:facebook_page/widgets/profile_avatar.dart';
 import 'package:flutter/material.dart';
-
 class ImageScreen extends StatefulWidget {
   final Story story;
   final int index;
@@ -15,80 +14,74 @@ class ImageScreen extends StatefulWidget {
 }
 
 class _ImageScreenState extends State<ImageScreen> {
+  late PageController _pageController;
   int _currentIndex = 0;
+
   @override
   void initState() {
     super.initState();
     _currentIndex = widget.index;
+    _pageController = PageController(initialPage: _currentIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Dismissible(key: Key(widget.story.user.name), direction: DismissDirection.down,onDismissed:(_)=> Navigator.pop(context) ,child:  Scaffold(
-      backgroundColor: Colors.black12,
-      appBar: AppBar(
-        title: Text(
-          stories[_currentIndex].user.name,
-          style: const TextStyle(fontWeight: FontWeight.bold),
-        ),
-        backgroundColor: Colors.black38,
-        leading: Padding(
-          padding: const EdgeInsets.all(12),
-          child: ProfileAvatar(
-            imageUrl: stories[_currentIndex].user.imageUrl,
+    return Dismissible(
+      key: Key(widget.story.user.name),
+      direction: DismissDirection.down,
+      onDismissed: (_) => Navigator.pop(context),
+      child: Scaffold(
+        backgroundColor: Colors.black12,
+        appBar: AppBar(
+          title: Text(
+            stories[_currentIndex].user.name,
+            style: const TextStyle(fontWeight: FontWeight.bold),
           ),
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsetsDirectional.symmetric(horizontal: 10),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: const Icon(Icons.close),
+          backgroundColor: Colors.black38,
+          leading: Padding(
+            padding: const EdgeInsets.all(12),
+            child: ProfileAvatar(
+              imageUrl: stories[_currentIndex].user.imageUrl,
             ),
           ),
-        ],
-      ),
-      body: Center(
-        child: GestureDetector(
-          onHorizontalDragEnd: (details) {
-            final double velocity = details.velocity.pixelsPerSecond.dx;
-            if (velocity > 0) {
-              setState(() {
-                _currentIndex =
-                    (_currentIndex - 1).clamp(0, stories.length - 1);
-              });
-            } else if (velocity < 0) {
-              setState(() {
-                _currentIndex =
-                    (_currentIndex + 1).clamp(0, stories.length - 1);
-              });
-            }
+          actions: [
+            Padding(
+              padding:
+                  const EdgeInsetsDirectional.symmetric(horizontal: 10),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: const Icon(Icons.close),
+              ),
+            ),
+          ],
+        ),
+        body: PageView.builder(
+          controller: _pageController,
+          itemCount: stories.length,
+          onPageChanged: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
           },
-          behavior: HitTestBehavior.opaque,
-          child: Image.network(
-            stories[_currentIndex].imageUrl,
-            loadingBuilder: (BuildContext context, Widget child,
-                ImageChunkEvent? loadingProgress) {
-              if (loadingProgress == null) {
-                return child;
-              } else {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
+          itemBuilder: (context, index) => GestureDetector(
+            onHorizontalDragEnd: (details) {
+              final double velocity = details.velocity.pixelsPerSecond.dx;
+              if (velocity > 0) {
+                _pageController.previousPage(
+                    duration: const Duration(milliseconds: 250),
+                    curve: Curves.easeInOut);
+              } else if (velocity < 0) {
+                _pageController.nextPage(
+                    duration: const Duration(milliseconds: 250),
+                    curve: Curves.easeInOut);
               }
             },
-            errorBuilder:
-                (BuildContext context, Object error, StackTrace? stackTrace) {
-              return const Center(
-                child: Text('Failed to load image'),
-              );
-            },
+            child: Image.network(stories[index].imageUrl),
           ),
         ),
       ),
-    )
     );
   }
 }
